1693c6d15f82da0eea654c89d0708d395e3aab95
kromunum <oumoon33@gmail.com>
2023-03-22 18:21:08 +0900

    guide

--- src/index.js
+++ src/index.js
@@ -19,11 +19,7 @@ let Index  = Loadable({ loader: () => import('./app/index')})
 let Layout = Loadable({ loader: () => import('./app/layout')})
 // let Product  = Loadable({ loader: () => import('./app/product')})
 // let About    = Loadable({ loader: () => import('./app/about')})
-<<<<<<< HEAD
 let Guide    = Loadable({ loader: () => import('./app/guide')})
-=======
-// let Guide    = Loadable({ loader: () => import('./app/guide')})
->>>>>>> bb8415101edbca6cbb2c48d66ef286454e97a957
 let News     = Loadable({ loader: () => import('./app/news')})
 let Shop     = Loadable({ loader: () => import('./app/shop')})
 // let Contact  = Loadable({ loader: () => import('./app/contact')})
@@ -42,15 +38,15 @@ root.render(
             <Route path="/"        element={<Index />} />
             {/*<Route path="/product" element={<Product />} />*/}
             {/*<Route path="/about"   element={<About />} />*/}
-<<<<<<< HEAD
+
             <Route path="/guide"   element={<Guide />} />
             <Route path="/news"    element={<News />} />
             <Route path="/shop"    element={<Shop />} />
-=======
+
             {/*<Route path="/guide"   element={<Guide />} />*/}
             <Route path="/news"    element={<News />} />
             <Route path="/shop"       element={<Shop />} />
->>>>>>> bb8415101edbca6cbb2c48d66ef286454e97a957
+
             {/*<Route path="/contact"    element={<Contact />} />*/}
 
             <Route path="/slide"  element={<Slide />} />
