36ac46be7e4426e75ff05f7223347070bad4b229
kromunum <oumoon33@gmail.com>
2023-03-22 15:12:27 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'mobx-react'
 import { configure } from 'mobx'
@@ -19,7 +19,11 @@ let Index  = Loadable({ loader: () => import('./app/index')})
 let Layout = Loadable({ loader: () => import('./app/layout')})
 // let Product  = Loadable({ loader: () => import('./app/product')})
 // let About    = Loadable({ loader: () => import('./app/about')})
+<<<<<<< HEAD
+let Guide    = Loadable({ loader: () => import('./app/guide')})
+=======
 // let Guide    = Loadable({ loader: () => import('./app/guide')})
+>>>>>>> bb8415101edbca6cbb2c48d66ef286454e97a957
 let News     = Loadable({ loader: () => import('./app/news')})
 let Shop     = Loadable({ loader: () => import('./app/shop')})
 // let Contact  = Loadable({ loader: () => import('./app/contact')})
@@ -38,9 +42,15 @@ root.render(
             <Route path="/"        element={<Index />} />
             {/*<Route path="/product" element={<Product />} />*/}
             {/*<Route path="/about"   element={<About />} />*/}
+<<<<<<< HEAD
+            <Route path="/guide"   element={<Guide />} />
+            <Route path="/news"    element={<News />} />
+            <Route path="/shop"    element={<Shop />} />
+=======
             {/*<Route path="/guide"   element={<Guide />} />*/}
             <Route path="/news"    element={<News />} />
             <Route path="/shop"       element={<Shop />} />
+>>>>>>> bb8415101edbca6cbb2c48d66ef286454e97a957
             {/*<Route path="/contact"    element={<Contact />} />*/}
 
             <Route path="/slide"  element={<Slide />} />
